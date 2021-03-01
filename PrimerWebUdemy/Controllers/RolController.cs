09dbb2ea3b63b23c8264860ca5cfd795d59b98b8
enrique-arevalo <kike04arevalo@gmail.com>
2021-02-28 23:30:03 -0600

    Usando siempre la libreria de ajax para poder persistir datos de manera asincronica. Se agrego font-awesome web y se ha dejado el desktop en carpetas de momento.

--- PrimerWebUdemy/Controllers/RolController.cs
+++ PrimerWebUdemy/Controllers/RolController.cs
@@ -59,5 +59,25 @@ namespace PrimerWebUdemy.Controllers
                 return PartialView("_TablaRol", listaRol);
             }
         }
+
+        public int Guardar(RolCLS oRolCLS, int titulo)
+        {
+            int rpta = 0;
+            using (var bd = new BDPasajeEntities())
+            {
+                if (titulo.Equals(1))  //si titulo es igual a uno, es un agregar
+                {
+                    Rol oRol = new Rol();
+                    oRol.NOMBRE = oRolCLS.nombre;
+                    oRol.DESCRIPCION = oRolCLS.descripcion;
+                    oRol.BHABILITADO = 1;
+
+                    bd.Rol.Add(oRol);
+                    rpta = bd.SaveChanges();  //Aparte de confirmar los cambios devuelve la cantidad de registros afectados
+                }
+            }
+
+            return rpta;
+        }
     }
 }
